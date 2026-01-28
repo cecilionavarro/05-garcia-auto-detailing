6df2642c452f3f2547912912da052e16b18e524f
cecilionavarro <navarroscecilio@gmail.com>
2026-01-28 02:51:22 -0800

    warnings fix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import Icon from "./Icon";
-import BlurText from "./ui/BlurText";
 import { FaTiktok } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
